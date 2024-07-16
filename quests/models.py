018173d311bed8907476cdd13e7c29fb9d6b9296
K-suk <kuma.kou.0201@gmail.com>
2024-07-17 03:08:01 +0900

    update

--- quests/models.py
+++ quests/models.py
@@ -6,7 +6,7 @@ import os
 import uuid
 
 class Tag(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     
