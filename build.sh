64281ff132901eb7ca78e99fda6390cc4c1ed746
K-suk <kuma.kou.0201@gmail.com>
2024-07-17 03:17:07 +0900

    put datas

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
 python manage.py superuser
-# python manage.py import_quests
+python manage.py import_quests
