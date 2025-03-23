65dbda366b79c0c52ca4b80dd7f7b22926301764
K-suk <kuma.kou.0201@gmail.com>
2025-03-22 18:36:31 -0700

    forget comma

--- tokyoquest/settings.py
+++ tokyoquest/settings.py
@@ -85,7 +85,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        'rest_framework.authentication.BasicAuthentication'
+        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
